cacf8706fea66576e44f031cd46cf7771dbfd814
Abdelrahmein Zeid <arzeid.1994@gmail.com>
2016-11-20 06:36:17 -0500

    shortly GUI center alighned and social buttons added

--- static/javascript/css_engine.js
+++ static/javascript/css_engine.js
@@ -2,12 +2,13 @@
 $(document).ready(function(){
     $(".dropdown").click(function(e){
         e.stopPropagation();
-        $(this).siblings(".dropdown").find(".dropdown-content").hide('slow');
+        $(this).siblings(".dropdown").find(".dropdown-content").hide();
         $(this).find(".dropdown-content").toggle();
     });
 });
 
 
 $(document).click(function(){
-    $(".dropdown-content").hide('slow'); 
+    //$(".dropdown-content").hide('slow'); 
+    $(".dropdown-content").hide(); 
 });
